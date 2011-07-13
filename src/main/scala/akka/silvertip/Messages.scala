4184b561f4e3b81656a89342bcf2f58eef456c25
Karim Osman <karim.osman@reaktor.fi>
2011-07-13 13:19:58 +0300

    akka-silvertip: Add SilvertipMessage base class for all message types
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- src/main/scala/akka/silvertip/Messages.scala
+++ src/main/scala/akka/silvertip/Messages.scala
@@ -2,12 +2,14 @@ package akka.silvertip
 
 import silvertip.Connection
 
-case object Connect
-case object Reconnecting
+abstract class SilvertipMessage
 
-case class Connected[T](connection: Connection[T])
-case class Disconnected[T](connection: Connection[T])
-case class GarbledMessage(message: String, data: Array[Byte])
-case class Idle[T](connection: Connection[T])
-case class Recv(connection: Connection[_], message: Any)
-case class Send(connection: Connection[_], message: Any)
+case object Connect extends SilvertipMessage
+case object Reconnecting extends SilvertipMessage
+
+case class Connected[T](connection: Connection[T]) extends SilvertipMessage
+case class Disconnected[T](connection: Connection[T]) extends SilvertipMessage
+case class GarbledMessage(message: String, data: Array[Byte]) extends SilvertipMessage
+case class Idle[T](connection: Connection[T]) extends SilvertipMessage
+case class Recv(connection: Connection[_], message: Any) extends SilvertipMessage
+case class Send(connection: Connection[_], message: Any) extends SilvertipMessage
