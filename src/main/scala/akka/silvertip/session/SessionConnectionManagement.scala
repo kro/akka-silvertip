3a7a6078622ae3da4264469452d14e323d2cd750
Karim Osman <karim.osman@reaktor.fi>
2011-07-13 19:25:31 +0300

    akka-silvertip: Add SessionConnectionManagement trait
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

---
+++ src/main/scala/akka/silvertip/session/SessionConnectionManagement.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package akka.silvertip.session
+
+import akka.actor.{Actor, ActorRef}
+import akka.silvertip._
+import silvertip.{Connection, Message, MessageParser}
+
+case object Start
+
+trait SessionConnectionManagement[T <: Message] extends SessionConnectionManagementConfig { this: Actor =>
+  protected var connection: ActorRef = _
+  override def preStart { self ! Start }
+  protected def sessionConnectionManagement: Receive = {
+    case Start => {
+      connection = Silvertip.createConnection(new ConnectionParameters(
+        new MessageParserFactory[T] {
+          def create = newMessageParser
+        },
+        Actor.actorOf(new Actor {
+          var session: Option[Session[T]] = None
+          def receive: Receive = {
+            case Connected(connection) => {
+              session = Some(newSession)
+              session.foreach(_.login(connection))
+            }
+            case Idle(connection) => {
+              session.foreach(_.keepAlive(connection))
+            }
+            case Recv(connection: Connection[_], message: Message) => {
+              session.foreach { session =>
+                session.receive(connection, message)
+              }
+            }
+            case Disconnected(connection: Connection[_]) => {
+              if (session.isDefined) self.reply(session.get.delayUntilReconnect) else 0
+            }
+            case message: SilvertipMessage => Unit
+          }
+        }).start, reconnectDelayMsec, hostname, port
+      ))
+    }
+  }
+  def newMessageParser: MessageParser[T]
+  def newSession: Session[T]
+}
+
+trait SessionConnectionManagementConfig {
+  def hostname: String
+  def port: Int
+  def reconnectDelayMsec: Int
+}
